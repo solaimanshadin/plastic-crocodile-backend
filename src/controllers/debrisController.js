750a957dd7a2b7c1fd61697093fbf5423700a68a
Solaiman Shadin <solaimanshadin@gmail.com>
2021-09-16 23:43:31 +0600

    [ADD] Debris data update API

--- src/controllers/debrisController.js
+++ src/controllers/debrisController.js
@@ -1,50 +1,74 @@
-const  {createResponse}   =   require('../utils/responseGenerate');
-const Debris   		  =   require('../models/Debris');
-
+const { createResponse } = require('../utils/responseGenerate');
+const Debris = require('../models/Debris');
 
 module.exports.createDebris = async (req, res, next) => {
-	try{
+	try {
 		const body = req.body;
 		const debris = new Debris(body);
 		await debris.save();
-		return res.status(201).json(createResponse(Debris, 'Data stored successfully!', false));
-	} catch(err) {
+		return res
+			.status(201)
+			.json(createResponse(Debris, 'Data stored successfully!', false));
+	} catch (err) {
+		next(err);
+	}
+};
+
+module.exports.updateDebris = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const debris = await Debris.updateOne(
+			{
+				_id: id,
+			},
+			{
+				...req.body,
+			}
+		);
+
+		return res
+			.status(204)
+			.json(createResponse(debris, 'Data updated successfully!', false));
+		
+	} catch (err) {
 		next(err);
 	}
 };
 
 module.exports.deleteDebris = async (req, res, next) => {
-	try{
+	try {
 		const { id } = req.params;
-		const Debris = await Debris.deleteOne({_id: id});
-		if(Debris.deletedCount) {
-			return res.status(200).json(createResponse(null, 'Data deleted successfully!', false));
+		const debris = await Debris.deleteOne({ _id: id });
+		if (debris.deletedCount) {
+			return res
+				.status(200)
+				.json(createResponse(null, 'Data deleted successfully!', false));
 		}
-		return res.status(404).json(createResponse(null, 'No  data found with this Id!', true));
-	} catch(err) {
+		return res
+			.status(404)
+			.json(createResponse(null, 'No  data found with this Id!', true));
+	} catch (err) {
 		next(err);
 	}
 };
 
 module.exports.getDebrisById = async (req, res, next) => {
-	try{
+	try {
 		const { id } = req.params;
-		const debris = await Debris.findOne({_id: id});
-		if(!debris) throw new Error('Nothing found with this id!');
+		const debris = await Debris.findOne({ _id: id });
+		if (!debris) throw new Error('Nothing found with this id!');
 		return res.json(createResponse(debris));
-	} catch(err){
+	} catch (err) {
 		next(err);
 	}
 };
 
 module.exports.getDebris = async (req, res, next) => {
-	try{
+	try {
 		const query = req.query;
 		const debris = await Debris.find(query);
 		return res.json(createResponse(debris));
-	} catch(err){
+	} catch (err) {
 		next(err);
 	}
 };
-
-
